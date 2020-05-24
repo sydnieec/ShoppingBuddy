f9c2775d5038245b3499edb6bd1b2ad111af4022
Sydnie Chau <sydniechau@Sydnies-MacBook-Pro.local>
2020-05-23 21:36:24 -0400

    added signup layout

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Grid, Row, Col } from "react-bootstrap";
+
 const SignUpPage = () => {
   return (
     <div>
@@ -49,6 +53,32 @@ const SignUpPage = () => {
       </nav>
 
       <h3>Signup page </h3>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control placeholder="ex 123-456-0000" />
+        </Form.Group>
+
+        <Form.Group id="formGridCheckbox">
+          <Form.Check type="checkbox" label="Allow Push Notifications" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
