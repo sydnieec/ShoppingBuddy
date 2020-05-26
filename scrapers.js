d46c350e9fb46fd6015f7cc70f9b6b6eb75f8d8c
Sydnie Chau <sydniechau@Sydnies-MacBook-Pro.local>
2020-05-25 20:49:14 -0400

    web scraper file added

---
+++ scrapers.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+async function scrapeProduct(url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="productTitle"]');
+    const txt = await el.getProperty("textContent");
+    const title = await txt.jsonValue();
+
+    const [el2] = await page.$x('//*[@id="price_inside_buybox"]');
+    const txt2 = await el2.getProperty("textContent");
+    const price = await txt2.jsonValue();
+
+    console.log({ title, price });
+
+    broswer.close();
+  } catch (error) {
+    console.log("error");
+  }
+}
+
+scrapeProduct(
+  "https://www.amazon.ca/gp/product/B07GK35P2C?pf_rd_r=7RK067TZRRWKJSHBKMC1&pf_rd_p=05326fd5-c43e-4948-99b1-a65b129fdd73"
+);
