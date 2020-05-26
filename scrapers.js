702d5c62dfd27b846d55605923fb00da19bd899c
Sydnie Chau <sydniechau@Sydnies-MacBook-Pro.local>
2020-05-26 16:41:13 -0400

    fixed webscraper input

--- scrapers.js
+++ scrapers.js
@@ -8,27 +8,40 @@ async function scrapeProduct(url) {
 
     const [el] = await page.$x('//*[@id="productTitle"]');
     const txt = await el.getProperty("textContent");
-    const title = await txt.jsonValue();
+    var title = await txt.jsonValue();
 
     const [el2] = await page.$x('//*[@id="price_inside_buybox"]');
     const txt2 = await el2.getProperty("textContent");
-    const price = await txt2.jsonValue();
+    var price = await txt2.jsonValue();
+
+    //strips the title and price of whitespaces and new lines
+    var title = title.replace(/\n/g, " ");
+    var price = price.replace(/\n/g, " ");
+    var title = title.replace(/\s\s+/g, " ");
+    var price = price.replace(/\s\s+/g, " ");
 
     console.log({ title, price });
 
     browser.close();
   } catch (error) {
+    //catches error if the price is on another div
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
     const [el] = await page.$x('//*[@id="productTitle"]');
     const txt = await el.getProperty("textContent");
-    const title = await txt.jsonValue();
+    var title = await txt.jsonValue();
 
     const [el2] = await page.$x('//*[@id="priceblock_dealprice"]');
     const txt2 = await el2.getProperty("textContent");
-    const price = await txt2.jsonValue();
+    var price = await txt2.jsonValue();
+
+    //strips the title and price of whitespaces and new lines
+    var title = title.replace(/\n/g, " ");
+    var price = price.replace(/\n/g, " ");
+    var title = title.replace(/\s\s+/g, " ");
+    var price = price.replace(/\s\s+/g, " ");
 
     console.log({ title, price });
 
@@ -39,5 +52,5 @@ async function scrapeProduct(url) {
 }
 
 scrapeProduct(
-  "https://www.amazon.ca/Miracle-Gro-AeroGarden-970293-0000-Sponges-50-Pack/dp/B008DY2U8K/ref=br_msw_pdt-6?_encoding=UTF8&smid=A3DWYIK6Y9EEQB&pf_rd_m=A3DWYIK6Y9EEQB&pf_rd_s=&pf_rd_r=40EKP0MEFB95WVAVJ29B&pf_rd_t=36701&pf_rd_p=fa81227e-a2f9-4a57-a776-2b0a0e402c3d&pf_rd_i=desktop"
+  "https://www.amazon.ca/gp/product/B07VG7PMC5/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1"
 );
